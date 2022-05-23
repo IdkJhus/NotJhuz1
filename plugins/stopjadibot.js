f33970be13f7ff8d8dba2d9c4e3b674ec77f8b89
IdkJhus <99780276+IdkJhus@users.noreply.github.com>
2022-05-23 16:00:48 -0600

    Update stopjadibot.js

--- plugins/stopjadibot.js
+++ plugins/stopjadibot.js
@@ -8,7 +8,7 @@ let handler  = async (m, { conn }) => {
 handler.help = ['detener','stop']
 handler.tags = ['General']
 handler.command = /^(detener|stop)$/i
-handler.owner = false
+handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = false
